163ce26b15eb4f5b17574ea5fb1ecd14f22a71ad
Seungyeon0918 <77165342+Seungyeon0918@users.noreply.github.com>
2021-08-01 16:26:53 +0900

    Add files via upload

---
+++ BOJ_13305.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i, n) for(int i = 0 ; i < n ; i++)
+#define FOR1(i, n) for(int i = 1 ; i <= n ; i++)
+
+int N;
+long long road[100001], price[100001];
+long long now_price; long long cost = 0;
+
+int main() {
+	cin >> N;
+	FOR(i, N - 1) cin >> road[i];
+	FOR(i, N) cin >> price[i];
+
+	now_price = price[0];
+
+	FOR(i, N-1) {			
+		cost += now_price * road[i];
+
+		if (now_price > price[i+1])
+			now_price = price[i+1];
+	}
+
+	cout << cost << '\n';
+}
